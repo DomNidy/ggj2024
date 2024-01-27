181fb2bcf00cf32a12dd30b93453e2a9d37fda4a
Tyler Sisley <dominicnidy3@gmail.com>
2024-01-26 21:48:27 -0500

    Initial commit

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Attatch to player game object
+public class PlayerController : MonoBehaviour
+{
+
+    public float moveSpeed = 5f;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
