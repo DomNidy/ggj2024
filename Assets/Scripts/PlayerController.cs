9f237ed053e79c16a26156c4ff8264c9768235a7
Tyler Sisley <dominicnidy3@gmail.com>
2024-01-26 22:04:59 -0500

    Implemented basic movement

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,19 +6,38 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
 
-    public float moveSpeed = 5f;
+    private float _moveSpeed = 5f;
+    public float moveSpeed
+    {
+        get
+        {
+            return _moveSpeed;
+        }
+        set
+        {
+            Debug.Log("Setting moveSpeed to " + value.ToString());
+            _moveSpeed = value;
+        }
+    }
 
 
 
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        float inputHorizontal = Input.GetAxisRaw("Horizontal") * moveSpeed * Time.deltaTime;
+        float inputVertical = Input.GetAxisRaw("Vertical") * moveSpeed * Time.deltaTime;
+
+        // Direction to move player in
+        Vector3 moveDirection = new Vector3(inputHorizontal, inputVertical);
+
+        transform.position += moveDirection;
+
     }
 }
---
+++ README.md
@@ -0,0 +1,3 @@
+# Global game jam 2024
+
+Project made with friends for the global game jam event.
